85e4cdf629b4551943cf00b739b9e7c4dfaad157
dingyongwei <2900178656@qq.com>
2016-09-21 00:13:45 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/dingyongwei/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.dingyongwei.miniweather;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by crazy on 2016-09-20-0020.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
